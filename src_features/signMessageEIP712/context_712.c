ce06c4cbc4ddc1caf02b68cc1e9cdd8205fce38f
Alexandre Paillier <alexandre.paillier@ledger.fr>
2025-05-28 09:44:32 +0200

    WIP context_712

--- src_features/signMessageEIP712/context_712.c
+++ src_features/signMessageEIP712/context_712.c
@@ -19,9 +19,6 @@ s_eip712_context *eip712_context = NULL;
  * @return a boolean indicating if the initialization was successful or not
  */
 bool eip712_context_init(void) {
-    // switch to legacy allocator
-    mem_legacy_init();
-
     // init global variables
     if ((eip712_context = MEM_ALLOC_AND_ALIGN_TYPE(*eip712_context)) == NULL) {
         apdu_response_code = APDU_RESPONSE_INSUFFICIENT_MEMORY;
@@ -67,8 +64,6 @@ void eip712_context_deinit(void) {
     path_deinit();
     field_hash_deinit();
     ui_712_deinit();
-    // switch back to SDK allocator
-    app_mem_init();
     eip712_context = NULL;
     reset_app_context();
 }
